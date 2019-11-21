fcf6e4838a9ab035df867dc2922224d05d49a427
Mustapha Benmbarek <mustapha.benmbarek@Gundam-2.local>
2019-11-21 23:24:04 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* @title: <<nodejs-helloworld>> program written in Node.js.
+ * @desc: A simple nodejs helloworld starter.
+ * @author: Mustapha Benmbarek.
+ * @Copyright © 2019 All rights reserved.
+ * @version: 1.0.0
+ */
+
+console.log('Hello World !');
